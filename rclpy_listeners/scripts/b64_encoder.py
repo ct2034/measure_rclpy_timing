e9d70b1f0ec113ed06ec30b489cdd0ec2549a57b
Christian Henkel <6976069+ct2034@users.noreply.github.com>
2023-05-22 18:27:37 +0200

    bits before the b64 were missing

--- rclpy_listeners/scripts/b64_encoder.py
+++ rclpy_listeners/scripts/b64_encoder.py
@@ -13,7 +13,7 @@ for file in os.listdir():
     try:
         with open(file, "rb") as img_file:
             b64_string = base64.b64encode(img_file.read())
-        print(f"![{file}]({b64_string.decode('utf-8')})")
+        print(f"![{file}](data:image/png;base64,{b64_string.decode('utf-8')})")
     except Exception as e:
         print(f"Error:\n```\n{e}\n```")
         print(f'```\n{traceback.format_exc()}\n```')
